6f269d24d918bbc1d121fcb2083332bc33ba40f1
Nicholas Lane <nicholas.lane@postgrad.manchester.ac.uk>
2024-10-07 05:43:32 -0500

    Updating the ConvNetworkAlgorithm_module.cc

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 cd /exp/uboone/app/users/nlane/production/KaonShortProduction04
 
 mrbsetenv
-mrb i -j4
+mrb i -j6
 
 cd /exp/uboone/app/users/nlane/production/KaonShortProduction04/srcs/ubana/ubana/searchingforstrangeness
