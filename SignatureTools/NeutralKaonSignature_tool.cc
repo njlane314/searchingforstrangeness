b13d4b4d5d627a4f6574b98d0ef67cc8fae37198
Nicholas Lane <nicholas.lane@postgrad.manchester.ac.uk>
2024-10-15 09:41:08 -0500

    Improving the functionality of the 'SignatureToolBase.h'

--- SignatureTools/NeutralKaonSignature_tool.cc
+++ SignatureTools/NeutralKaonSignature_tool.cc
@@ -14,14 +14,16 @@ public:
     NeutralKaonSignature(const fhicl::ParameterSet& pset);
     ~NeutralKaonSignature(){};
     
-    void configure(fhicl::ParameterSet const & pset);
-    void identifySignalParticles(art::Event const& evt, 
-                                         std::vector<Particle>& particle_elements, 
-                                         bool& found_signature, 
-                                         std::array<float, 3>& nu_vtx);
-    
+    void configure(fhicl::ParameterSet const & pset) override;
+
+protected:
+    void findSignature(art::Event const& evt, std::vector<Particle>& particle_elements, bool& found_signature) override;
+
 private:
-    
+    art::InputTag _HitProducer;
+    art::InputTag _MCPproducer;
+    art::InputTag _MCTproducer;
+    art::InputTag _BacktrackTag;
 };
 
 NeutralKaonSignature::NeutralKaonSignature(const fhicl::ParameterSet& pset)
@@ -29,31 +31,22 @@ NeutralKaonSignature::NeutralKaonSignature(const fhicl::ParameterSet& pset)
     , _MCPproducer{pset.get<art::InputTag>("MCPproducer", "largeant")}
     , _MCTproducer{pset.get<art::InputTag>("MCTproducer", "generator")}
     , _BacktrackTag{pset.get<art::InputTag>("BacktrackTag", "gaushitTruthMatch")}
-    , _PionThreshold{pset.get<float>("PionThreshold", 0.1)}
-    , _MuonThreshold{pset.get<float>("MuonThreshold", 0.1)}
-    , _ProtonThreshold{pset.get<float>("ProtonThreshold", 0.1)}
 {
+    configure(pset); 
 }
 
 void NeutralKaonSignature::configure(fhicl::ParameterSet const & pset)
 {
+    SignatureToolBase::configure(pset);
 }
 
-void NeutralKaonSignature::identifySignalParticles(art::Event const& evt, 
-                                         std::vector<Particle>& particle_elements, 
-                                         bool& found_signature, 
-                                         std::array<float, 3>& nu_vtx)
+void NeutralKaonSignature::findSignature(art::Event const& evt, std::vector<Particle>& particle_elements, bool& found_signature)
 {
-    particle_elements.clear();
-    found_signature = false;
-    std::fill(nu_vtx.begin(), nu_vtx.end(), 0.0f);
-
     auto const &mct_h = evt.getValidHandle<std::vector<simb::MCTruth>>(_MCTproducer);
     auto const &mcp_h = evt.getValidHandle<std::vector<simb::MCParticle>>(_MCPproducer);
 
     std::map<int, art::Ptr<simb::MCParticle>> mcp_map;
-    for (size_t d = 0; d < mcp_h->size(); d++)
-    {
+    for (size_t d = 0; d < mcp_h->size(); d++) {
         const art::Ptr<simb::MCParticle> mcp(mcp_h, d);
         mcp_map[mcp->TrackId()] = mcp;
     }
@@ -62,61 +55,41 @@ void NeutralKaonSignature::identifySignalParticles(art::Event const& evt,
     if (!mct.NeutrinoSet()) return;
 
     auto const &neutrino = mct.GetNeutrino();
-    auto const &nu = neutrino.Nu();
-    common::True2RecoMappingXYZ(nu.T(), nu.Vx(), nu.Vy(), nu.Vz(), nu_vtx.data());
+    common::True2RecoMappingXYZ(neutrino.Nu().T(), neutrino.Nu().Vx(), neutrino.Nu().Vy(), neutrino.Nu().Vz(), nu_vtx.data());
 
     if (neutrino.CCNC() != simb::kCC) return;
-    const simb::MCParticle& lepton = neutrino.Lepton();
 
-    if (abs(lepton.PdgCode()) != muon->PdgCode() || lepton.Momentum().Vect().Mag() < _MuonThreshold) return;
-
-    for (const auto &t_part : *mcp_h) 
-    {
-        if (abs(t_part.PdgCode()) == muon->PdgCode() && t_part.Process() == "primary" && t_part.Momentum().Vect().Mag() > _MuonThreshold)
-        {
-            Particle muon_particle;
-            muon_particle.pdg_code = t_part.PdgCode();
-            muon_particle.track_id = t_part.TrackId();
-            muon_particle.momentum = {static_cast<float>(t_part.Px()), static_cast<float>(t_part.Py()), static_cast<float>(t_part.Pz())};
-            particle_elements.push_back(muon_particle);
+    for (const auto &t_part : *mcp_h) {
+        if (abs(t_part.PdgCode()) == 13 && t_part.Process() == "primary" && aboveThreshold(t_part)) {
+            fillParticle(t_part, particle_elements);
         }
 
-        if (abs(t_part.PdgCode()) == neutral_kaon->PdgCode() && t_part.Process() == "primary" && t_part.EndProcess() == "Decay" && t_part.NumberDaughters() == 1 && !found_signature) 
-        {
-            std::vector<art::Ptr<simb::MCParticle>> dtrs = common::GetDaughters(mcp_map.at(t_part.TrackId()), mcp_map);
-            if (dtrs.size() != 1) continue; 
-
-            auto g_part = dtrs.at(0);
-            if (g_part->PdgCode() == kaon_short->PdgCode() && g_part->Process() == "Decay" && g_part->EndProcess() == "Decay" && g_part->NumberDaughters() == 2 && !found_signature)
-            {
-                auto daughters = common::GetDaughters(mcp_map.at(g_part->TrackId()), mcp_map);
-                if (daughters.size() != 2) continue;
-
+        if (abs(t_part.PdgCode()) == 310 && t_part.Process() == "primary" && t_part.EndProcess() == "Decay" && t_part.NumberDaughters() == 1 && !found_signature) {
+            auto daughters = common::GetDaughters(mcp_map.at(t_part.TrackId()), mcp_map);
+            if (daughters.size() == 2) {
                 std::vector<int> exp_dtrs = {-211, 211};
                 std::vector<int> fnd_dtrs;
-
+                
                 for (const auto &dtr : daughters) 
                     fnd_dtrs.push_back(dtr->PdgCode());
-                    
+
                 std::sort(exp_dtrs.begin(), exp_dtrs.end());
                 std::sort(fnd_dtrs.begin(), fnd_dtrs.end());
+
                 if (fnd_dtrs == exp_dtrs) 
                 {
-                    for (const auto &dtr : daughters) 
-                    {
-                        Particle particle;
-                        particle.pdg_code = dtr->PdgCode();
-                        particle.track_id = dtr->TrackId();
-                        particle.momentum = {static_cast<float>(dtr->Px()), static_cast<float>(dtr->Py()), static_cast<float>(dtr->Pz())};
-                        
-                        particle_elements.push_back(particle);
-                    }
-
-                    found_signature = std::all_of(daughters.begin(), daughters.end(), [&](const auto& dtr) {
-                        return dtr->Momentum().Vect().Mag() >= _PionThreshold;
+                    bool all_above_threshold = std::all_of(daughters.begin(), daughters.end(), [&](const auto& dtr) {
+                        return checkThreshold(*dtr);
                     });
 
-                    if (found_signature) break;
+                    if (all_above_threshold) 
+                    {
+                        found_signature = true;
+                        for (const auto &dtr : daughters) 
+                            fillParticle(dtr, particle_elements);
+
+                        break;
+                    }
                 }
             }
         }
--- SignatureTools/SignatureToolBase.h
+++ SignatureTools/SignatureToolBase.h
@@ -29,11 +29,49 @@ public:
 
     virtual ~SignatureToolBase() noexcept = default;
     
-    void configure(const fhicl::ParameterSet&){};
-    virtual void identifySignalParticles(art::Event const& evt, 
-                                         std::vector<Particle>& particle_elements, 
-                                         bool& found_signature, 
-                                         std::array<float, 3>& nu_vtx) = 0;
+    void configure(const fhicl::ParameterSet&){
+        _thresh_map[211] = pset.get<float>("PionThreshold", 0.1);
+        _thresh_map[13] = pset.get<float>("MuonThreshold", 0.1);
+        _thresh_map[2212] = pset.get<float>("ProtonThreshold", 0.1);
+        _thresh_map[321] = pset.get<float>("KaonThreshold", 0.1);
+    };
+
+    bool identifySignalParticles(art::Event const& evt, std::vector<Particle>& particle_elements)
+    {
+        bool found_signature = false;
+        particle_elements.clear();
+
+        this->findSignature(evt, particle_elements, found_signature);
+
+        if (!found_signature)  
+            particle_elements.clear();
+
+        return found_signature;
+    }
+
+    bool aboveThreshold(const simb::MCParticle& mc_particle) const {
+        float mom_mag = mc_particle.Momentum().Vect().Mag()
+        int abs_pdg = std::abs(mc_particle.PdgCode());
+
+        if (it != _thresh_map.end()) {
+            return mom_mag > it->second;
+        }
+
+        return false;
+    }
+
+protected:
+    std::unordered_map<int, float> _thresh_map;
+
+    void fillParticle(const art::Ptr<simb::MCParticle>& mc_particle, std::vector<Particle>& particle_elements) {
+        Particle particle;
+        particle.pdg_code = mc_particle->PdgCode();
+        particle.track_id = mc_particle->TrackId();
+        particle.momentum = {static_cast<float>(mc_particle->Px()), static_cast<float>(mc_particle->Py()), static_cast<float>(mc_particle->Pz())};
+        particle_elements.push_back(particle);
+    }
+
+    virtual void findSignature(art::Event const& evt, std::vector<Particle>& particle_elements, bool& found_signature) = 0;
 };
 
 } 
