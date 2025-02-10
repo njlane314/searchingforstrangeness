73a2e95e44734ea6b6589fcd76057ef796891c7c
Nicholas Lane <nicholas.lane@postgrad.manchester.ac.uk>
2025-02-10 09:34:12 -0600

    Bug fix

--- scripts/do_hadd.sh
+++ scripts/do_hadd.sh
@@ -3,9 +3,9 @@
 outdir=/exp/uboone/data/users/nlane/strangeness/ana/
 mkdir -p "${outdir}"
 
-treename="${outdir}/analysis_output.root"
+treename="${outdir}nlane_prod_strange_resample_fhc_run2_fhc_reco2_reco2_trainingimage_background_lambdamuon_ana.root"
 
-rootdir=/pnfs/uboone/scratch/users/nlane/kaon_dl/v08_00_00_82/nlane_prod_strange_resample_fhc_run2_fhc_reco2_reco2_trainingimage_signal_lambdamuon/make_csv/out/
+rootdir=/pnfs/uboone/scratch/users/nlane/kaon_dl/v08_00_00_82/nlane_prod_strange_resample_fhc_run2_fhc_reco2_reco2_trainingimage_background_lambdamuon/out/
 
 onelinefilelist=$(find "$rootdir" -type f -name "*.root" | tr '\n' ' ')
 
