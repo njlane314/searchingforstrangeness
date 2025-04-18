b76f7ed066b92b8d0d0e4ebacf19b4340b3c8f0b
Nicholas Lane <nicholas.lane@postgrad.manchester.ac.uk>
2025-04-18 08:24:31 -0500

    Adding further to README

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,33 +1,17 @@
 unsetup_all
 source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone_mcc9.sh
-setup uboonecode v08_00_00_82 -q "e17:prof"
+setup larsoft v10_04_07 -q e26:prof
+setup libtorch v2_1_1a -q e26
+setup mrb
 
-#ups list -aK+ libtorch
-#setup libtorch v1_0_1_mkldnn -q Linux64bit+3.10-2.17:e17:prof
-#setup libtorch v1_0_1_nonuma -q Linux64bit+3.10-2.17:e17:prof
-setup libtorch v1_0_1 -q Linux64bit+3.10-2.17:e17:prof
-
-unsetup mrb
-setup mrb -o
-
-source /exp/uboone/app/users/nlane/production/KaonShortProduction04/localProducts_larsoft_v08_05_00_24_e17_prof/setup
+source /exp/uboone/app/users/nlane/production/KaonShortProduction05/localProducts_larsoft_v10_04_07_e26_prof/setup
 mrbslp
 
 alias cdt="cd ${MRB_TOP}"
 
-export SEARCH_TOP="/exp/uboone/app/users/nlane/production/KaonShortProduction04/srcs/ubana/ubana/searchingforstrangeness"
+export SEARCH_TOP="/exp/uboone/app/users/nlane/production/KaonShortProduction05/srcs/ubana/ubana/searchingforstrangeness"
 alias cds="cd ${SEARCH_TOP}"
 
-#export TORCH_DIR=/cvmfs/uboone.opensciencegrid.org/products/libtorch/v1_0_1_nonuma/Linux64bit+3.10-2.17-e17-prof/share/cmake/Torch/TorchConfig.cmake
-export TORCH_DIR=/cvmfs/uboone.opensciencegrid.org/products/libtorch/v1_0_1/Linux64bit+3.10-2.17-e17-prof/lib/python2.7/site-packages/torch/share/cmake/Torch
+export TORCH_DIR=/cvmfs/uboone.opensciencegrid.org/products/libtorch/v2_1_1a/Linux64bit+3.10-2.17-e26/lib/python3.8/site-packages/torch/share/cmake/Torch
 export CMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH:$TORCH_DIR"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/larsoft.opensciencegrid.org/products/python/v2_7_14b/Linux64bit+3.10-2.17/lib
-
-case "$USER" in
-    nlane)
-        export TARBALL_NAME="StrangenessCode.tar"
-        export TARBALL_DEST="/pnfs/uboone/resilient/users/nlane/NeutralKaon/tarballs/"
-        ;;
-esac
-
-git pull
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/larsoft.opensciencegrid.org/products/python/v3_8_10/Linux64bit+3.10-2.17/lib
