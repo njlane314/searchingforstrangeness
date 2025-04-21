6bb5233a303f9719710114e2b85a3b820f9b258f
Nicholas Lane <nicholas.lane@postgrad.manchester.ac.uk>
2025-04-21 11:24:59 -0500

    Adding FiducialVolumeSelector_tool.cc

--- SelectionTools/FiducialSelection_tool.cc
+++ SelectionTools/FiducialSelection_tool.cc
@@ -3,6 +3,7 @@
 #include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
 #include "larcore/Geometry/Geometry.h"
 #include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "../CommonDefs/SCECorrectionsFuncs.h" 
 
 namespace selection
 {
@@ -15,18 +16,20 @@ namespace selection
         bool selectEvent(art::Event const& e, const std::vector<ProxyPfpElem_t>& pfp_pxy_v);
         void setBranches(TTree* _tree);
         void resetTTree(TTree* _tree);
+        
+        bool isFiducial(const double x[3]) const;
 
     private:
-        double _fiducial_x_min, _fiducial_x_max;
-        double _fiducial_y_min, _fiducial_y_max;
-        double _fiducial_z_min, _fiducial_z_max;
+        double m_fidvolXstart, m_fidvolXend;
+        double m_fidvolYstart, m_fidvolYend;
+        double m_fidvolZstart, m_fidvolZend;
 
         art::ServiceHandle<geo::Geometry> _geometry;
         art::ServiceHandle<detinfo::DetectorPropertiesService> _detprop;
 
-        double m_x_centroid; 
-        double m_y_centroid;
-        double m_z_centroid;
+        double m_reco_nu_vtx_x; 
+        double m_reco_nu_vtx_y;
+        double m_reco_nu_vtx_z;
     };
 
     FiducialSelection::FiducialSelection(const fhicl::ParameterSet& pset) {
@@ -34,83 +37,70 @@ namespace selection
     }
 
     void FiducialSelection::configure(fhicl::ParameterSet const & pset) {
-        _fiducial_x_min = pset.get<double>("fiducial_x_min");
-        _fiducial_x_max = pset.get<double>("fiducial_x_max");
-        _fiducial_y_min = pset.get<double>("fiducial_y_min");
-        _fiducial_y_max = pset.get<double>("fiducial_y_max");
-        _fiducial_z_min = pset.get<double>("fiducial_z_min");
-        _fiducial_z_max = pset.get<double>("fiducial_z_max");
+        m_fidvolXstart = pset.get<double>("fidvolXstart");
+        m_fidvolXend = pset.get<double>("fidvolXend");
+        m_fidvolYstart = pset.get<double>("fidvolYstart");
+        m_fidvolYend = pset.get<double>("fidvolYend");
+        m_fidvolZstart = pset.get<double>("fidvolZstart");
+        m_fidvolZend = pset.get<double>("fidvolZend");
     }
 
     void FiducialSelection::setBranches(TTree* _tree) {
-        _tree->Branch("x_centroid", &m_x_centroid, "x_centroid/D");
-        _tree->Branch("y_centroid", &m_y_centroid, "y_centroid/D");
-        _tree->Branch("z_centroid", &m_z_centroid, "z_centroid/D");
+        _tree->Branch("reco_nu_vtx_x", &m_reco_nu_vtx_x, "reco_nu_vtx_x/D"); 
+        _tree->Branch("reco_nu_vtx_y", &m_reco_nu_vtx_y, "reco_nu_vtx_y/D"); 
+        _tree->Branch("reco_nu_vtx_z", &m_reco_nu_vtx_z, "reco_nu_vtx_z/D"); 
     }
 
     void FiducialSelection::resetTTree(TTree* _tree) {
-        m_x_centroid = std::numeric_limits<double>::min(); 
-        m_y_centroid = std::numeric_limits<double>::min();
-        m_z_centroid = std::numeric_limits<double>::min();
+        m_reco_nu_vtx_x = std::numeric_limits<double>::min(); 
+        m_reco_nu_vtx_y = std::numeric_limits<double>::min();
+        m_reco_nu_vtx_z = std::numeric_limits<double>::min();
+    }
+
+    bool FiducialSelection::isFiducial(const double x[3]) const {
+        geo::TPCGeo const &thisTPC = _geometry->TPC();
+        geo::BoxBoundedGeo theTpcGeo = thisTPC.ActiveBoundingBox();
+        std::vector<double> bnd = {theTpcGeo.MinX(), theTpcGeo.MaxX(), theTpcGeo.MinY(), theTpcGeo.MaxY(), theTpcGeo.MinZ(), theTpcGeo.MaxZ()};
+        bool is_x = x[0] > (bnd[0] + m_fidvolXstart) && x[0] < (bnd[1] - m_fidvolXend);
+        bool is_y = x[1] > (bnd[2] + m_fidvolYstart) && x[1] < (bnd[3] - m_fidvolYend);
+        bool is_z = x[2] > (bnd[4] + m_fidvolZstart) && x[2] < (bnd[5] - m_fidvolZend);
+        return is_x && is_y && is_z;
     }
 
     bool FiducialSelection::selectEvent(art::Event const& e, const std::vector<ProxyPfpElem_t>& pfp_pxy_v) {
-        std::vector<art::Ptr<recob::Hit>> slice_hits;
         for (const auto& pfp_pxy : pfp_pxy_v) {
-            auto hits = pfp_pxy.get<recob::Hit>();
-            slice_hits.insert(slice_hits.end(), hits.begin(), hits.end());
+            if (pfp_pxy->IsPrimary()) {
+                auto vertices = pfp_pxy.get<recob::Vertex>();
+                if (vertices.size() == 1) {
+                    const auto& vertex = vertices[0];
+                    double xyz[3] = {};
+                    vertex->XYZ(xyz);
+
+                    float x = static_cast<float>(xyz[0]);
+                    float y = static_cast<float>(xyz[1]);
+                    float z = static_cast<float>(xyz[2]);
+
+                    searchingfornues::ApplySCECorrectionXYZ(x, y, z);
+
+                    xyz[0] = static_cast<double>(x);
+                    xyz[1] = static_cast<double>(y);
+                    xyz[2] = static_cast<double>(z);
+
+                    m_reco_nu_vtx_x = xyz[0];
+                    m_reco_nu_vtx_y = xyz[1];
+                    m_reco_nu_vtx_z = xyz[2];
+
+                    bool in_fiducial = isFiducial(xyz);
+
+                    return in_fiducial;
+                }
+            }
         }
 
-        if (slice_hits.empty()) {
-            m_x_centroid = std::numeric_limits<double>::min();
-            m_y_centroid = std::numeric_limits<double>::min();
-            m_z_centroid = std::numeric_limits<double>::min();
-            return false;
-        }
-
-        double total_charge = 0.0;
-        double x_sum = 0.0, y_sum = 0.0, z_sum = 0.0;
-
-        for (const auto& hit : slice_hits) {
-            double charge = hit->Integral();
-            total_charge += charge;
-
-            unsigned int wire = hit->WireID().Wire;
-            unsigned int plane = hit->WireID().Plane;
-            double time = hit->PeakTime();
-
-            double x = _detprop->ConvertTicksToX(time, plane);
-
-            geo::WireID wire_id(hit->WireID());
-            auto wire_pos = _geometry->WireIDToWireGeo(wire_id).GetCenter();
-            double y = wire_pos.Y();
-            double z = wire_pos.Z();
-
-            x_sum += charge * x;
-            y_sum += charge * y;
-            z_sum += charge * z;
-        }
-
-        if (total_charge <= 0) {
-            m_x_centroid = std::numeric_limits<double>::min();
-            m_y_centroid = std::numeric_limits<double>::min();
-            m_z_centroid = std::numeric_limits<double>::min();
-            return false;
-        }
-
-        double x_centroid = x_sum / total_charge;
-        double y_centroid = y_sum / total_charge;
-        double z_centroid = z_sum / total_charge;
-
-        m_x_centroid = x_centroid;  
-        m_y_centroid = y_centroid;
-        m_z_centroid = z_centroid;
-
-        bool in_fiducial = (x_centroid >= _fiducial_x_min && x_centroid <= _fiducial_x_max &&
-                            y_centroid >= _fiducial_y_min && y_centroid <= _fiducial_y_max &&
-                            z_centroid >= _fiducial_z_min && z_centroid <= _fiducial_z_max);
-
-        return in_fiducial;
+        m_reco_nu_vtx_x = std::numeric_limits<double>::min();
+        m_reco_nu_vtx_y = std::numeric_limits<double>::min();
+        m_reco_nu_vtx_z = std::numeric_limits<double>::min();
+        return false;
     }
 }
 
