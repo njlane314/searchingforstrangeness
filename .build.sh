0f276b7109b55a4ef31ecc8bb1d17b11477e00ca
Nicholas <107313324+njlane314@users.noreply.github.com>
2025-09-03 17:02:26 +0100

    Add script to tar build and assets

--- .build.sh
+++ .build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+set -e
 
 export WRK_DIR=$(pwd)
-cd $MRB_TOP
+cd "$MRB_TOP"
 
 mrbsetenv
 mrb i -j8
 
-cd $WRK_DIR
-
-"$WRK_DIR/scripts/tar.sh"
+cd "$WRK_DIR"
+"$WRK_DIR/tar_artifacts.sh"
