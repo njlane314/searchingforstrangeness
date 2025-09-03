655c330691db06a1c44558d4659e7fcde23a6797
Nicholas <107313324+njlane314@users.noreply.github.com>
2025-09-03 14:54:08 +0100

    Tar build products on successful build

--- .build.sh
+++ .build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+set -e
+
 export WRK_DIR=$(pwd)
-cd $MRB_TOP
+cd "$MRB_TOP"
 
 mrbsetenv
 mrb i -j8
 
-cd $WRK_DIR
+cd "$WRK_DIR"
 
+# Package the LArSoft local installation and assets if the build succeeds
+"$WRK_DIR/scripts/tar.sh"
