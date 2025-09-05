1edc87050e9ab2d0b71d849448bc184a0379d249
Nicholas Lane <nicholas.lane@postgrad.manchester.ac.uk>
2025-09-05 05:04:59 -0500

    Update

--- .setup.sh
+++ .setup.sh
@@ -27,4 +27,4 @@ which jobsub_submit
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/cvmfs/larsoft.opensciencegrid.org/products/python/v2_7_14b/Linux64bit+3.10-2.17/lib"
 
 kx509
-voms-proxy-init -noregen -voms fermilab:/fermilab/uboone/Role=Analysis
+htgettoken -a htvaultprod.fnal.gov -i uboone
