1fb02a4c2a1b407612472ebc937b8f3f751de147
Nicholas <107313324+njlane314@users.noreply.github.com>
2025-11-01 21:30:31 +0000

    Reorder product dictionary groupings

--- Products/classes.h
+++ Products/classes.h
@@ -1,22 +1,23 @@
+#include <vector>
+
 #include "canvas/Persistency/Common/Wrapper.h"
 
 #include "Products/ImageProducts.h"
 #include "Products/InferencePerf.h"
 #include "Products/InferencePred.h"
 
-#include <vector>
-
+// Image products
 template class std::vector<image::ImageProduct>;
 template class art::Wrapper<image::ImageProduct>;
 template class art::Wrapper<std::vector<image::ImageProduct>>;
 
-template class art::Wrapper<image::InferencePerfProduct>;
-
-template class art::Wrapper<image::InferencePredProduct>;
-
+// Inference performance products
 template class std::vector<image::ModelPerf>;
 template class art::Wrapper<std::vector<image::ModelPerf>>;
+template class art::Wrapper<image::InferencePerfProduct>;
 
+// Inference prediction products
 template class std::vector<image::ModelPred>;
 template class art::Wrapper<std::vector<image::ModelPred>>;
+template class art::Wrapper<image::InferencePredProduct>;
 
