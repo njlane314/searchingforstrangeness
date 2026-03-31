1d525e7982ffe6787d539ce336904463371bb260
njlane314 <njlane314@gmail.com>
2026-03-31 12:23:22 +0100

    Fix ROOT dictionary header lookup during build

--- .build.sh
+++ .build.sh
@@ -3,6 +3,9 @@ export WRK_DIR=$(pwd)
 cd $MRB_TOP
 
 mrbsetenv
+# ROOT dictionary checks resolve headers through ROOT_INCLUDE_PATH rather than
+# the compiler include flags used during the build.
+export ROOT_INCLUDE_PATH="$WRK_DIR${ROOT_INCLUDE_PATH:+:$ROOT_INCLUDE_PATH}"
 mrb i -j8
 
 cd $WRK_DIR
